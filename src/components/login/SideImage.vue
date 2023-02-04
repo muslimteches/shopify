04a83dc499e406eebfd1cb57affd27621a07277c
amin <m.aminsh1379@gmail.com>
2023-02-04 07:21:31 -0800

    taklif

---
+++ src/components/login/SideImage.vue
@@ -0,0 +1,12 @@
+<template>
+  <div
+    style="height: 100%"
+    class="bg-primary text-center row items-center justify-center"
+  >
+    <h3 class="no-margin text-weight-bold text-white">
+      به فروشگاه <span class="text-secondary">shopify</span> خوش آمدید
+    </h3>
+  </div>
+</template>
+
+<script setup></script>
---
+++ src/pages/RegisterPage.vue
@@ -0,0 +1,14 @@
+<template>
+  <q-page padding>
+    <!-- content -->
+    <div class="row full-width" style="min-height: 100vh">
+      <div class="col-6"><TopTitle /></div>
+      <div class="col-6"><SideImage /></div>
+    </div>
+  </q-page>
+</template>
+
+<script setup>
+import SideImage from "src/components/login/SideImage.vue";
+import TopTitle from "src/components/login/TopTitle.vue";
+</script>
