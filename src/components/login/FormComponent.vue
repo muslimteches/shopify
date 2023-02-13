b6c4f48e4d7c9c64c7d84d88473c4f018fab614d
milad4444 <miladsarli4444@gmail.com>
2023-02-13 20:51:09 +0330

    login page

--- src/components/login/FormComponent.vue
+++ src/components/login/FormComponent.vue
@@ -1,12 +1,26 @@
 <template>
-  <div>My component</div>
+  <div>
+    <q-card class="my-card">
+      <q-card-section>
+        <q-form @submit="onSubmit" class="q-gutter-md">
+          <q-input square outlined v-model="username" label="Square outlined" />
+          <q-input square outlined v-model="password" label="Square outlined" />
+          <div>
+            <q-btn label="Submit" type="submit" color="primary" />
+          </div>
+        </q-form>
+      </q-card-section>
+    </q-card>
+
+
+  </div>
 </template>
 
-<script>
-export default {
-  // name: 'ComponentName',
-  setup () {
-    return {}
-  }
+<script setup>
+import { ref } from 'vue';
+const username = ref(null)
+const password = ref(null)
+function onSubmit () {
+  alert('sabtenam ok shod')
 }
 </script>
