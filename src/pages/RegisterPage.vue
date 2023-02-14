7aeb746dd1fa7abc74cb5fbca2480f555e3cfa68
twylybawas <twylybwas@gmail.com>
2023-02-14 17:08:06 +0330

    changr

--- src/pages/RegisterPage.vue
+++ src/pages/RegisterPage.vue
@@ -1,19 +1,22 @@
 <template>
   <q-page>
-    components: { TopTitle },page>
     <!-- content -->
-    <div class="row full-width" style="min-height: 100vh">
+    <div class="row full-width justify-center bg-svg" style="min-height: 100vh">
       <div class="col-6">
         <TopTitle />
-      </div>
-      <div class="col-6">
-        <sideImage />
       </div>
     </div>
   </q-page>
 </template>
 
 <script setup>
-import sideImage from "src/components/login/SideImage.vue";
 import TopTitle from "src/components/login/TopTitle.vue";
 </script>
+<style lang="scss">
+.bg-svg {
+  background-color: #ffefe0;
+  background-image: url("deta:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/s'");
+  background-attachment: fixed;
+  background-size: cover;
+}
+</style>
