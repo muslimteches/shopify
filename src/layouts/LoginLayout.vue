dbc3650fc4c90c08f724aecd6087d7f4a4327676
twylybawas <twylybwas@gmail.com>
2023-02-15 20:25:00 +0330

    last changes

--- src/layouts/LoginLayout.vue
+++ src/layouts/LoginLayout.vue
@@ -1,6 +1,9 @@
 <template>
   <q-layout view="hHh Lpr fFf">
     <!-- Be sure to play with the Layout demo on docs -->
+    <q-page-container>
+      <router-view />
+    </q-page-container>
   </q-layout>
 </template>
 
