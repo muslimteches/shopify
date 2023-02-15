aff3812206b5991f86288a4b8e4b18fb327d0135
twylybawas <twylybwas@gmail.com>
2023-02-15 17:03:31 +0330

    first chenge

--- src/components/login/FormComponent.vue
+++ src/components/login/FormComponent.vue
@@ -1,12 +1,28 @@
 <template>
-  <div>My component</div>
+  <div>
+    <q-card class="my-card">
+      <q-card-section>
+        <q-form @submit="onSubmit" class="q-gutter-md">
+          <q-input
+            dense
+            v-model="username"
+            label="نام کاربری (شماره تلفن یا ایمیل)"
+          />
+          <q-input dense v-model="password" label="رمز عبور" />
+          <div>
+            <q-btn label="Submit" type="submit" color="primary" />
+          </div>
+        </q-form>
+      </q-card-section>
+    </q-card>
+  </div>
 </template>
 
-<script>
-export default {
-  // name: 'ComponentName',
-  setup () {
-    return {}
-  }
+<script setup>
+import { ref } from "vue";
+const username = ref(null);
+const password = ref(null);
+function onSubmit() {
+  alert("تمام دوغون");
 }
 </script>
--- src/components/login/TopTitle.vue
+++ src/components/login/TopTitle.vue
@@ -1,11 +1,19 @@
 <template>
   <div class="row full-width justify-center q-pt-xl">
     <div class="col-12">
-      <h4 class="text-center text-weight-bold text-grey-9">دنبال من میگردی</h4>
+      <h4 class="text-center text-weight-bold text-white no-margin">
+        دنبال من میگردی
+      </h4>
     </div>
     <div class="col-12 row justify-center items-center">
-      <p class="no-margin">آیا قبلا ثبت نام کرده اید؟</p>
-      <q-btn dense flat color="primery" class="text-weight-bold" label="ورود" />
+      <p class="no-margin text-white">آیا قبلا ثبت نام کرده اید؟</p>
+      <q-btn
+        dense
+        flat
+        color="white"
+        class="q-mx-sm q-px-sm text-weight-bold"
+        label="ورود"
+      />
     </div>
   </div>
 </template>
