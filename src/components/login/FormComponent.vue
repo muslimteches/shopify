84385135ccf31deaee79765c643ed7d1b6198799
unknown <you@example.com>
2023-02-25 16:28:24 +0330

    more3

--- src/components/login/FormComponent.vue
+++ src/components/login/FormComponent.vue
@@ -3,9 +3,16 @@
     <q-card class="my-card">
       <q-card-section>
         <q-form @submit="onSubmit" class="q-gutter-md">
-          <q-input dense v-model="username" label="نام و نام خانوادگی" />
+          <q-input
+            outlined
+            dense
+            v-model="username"
+            label="نام و نام خانوادگی"
+          />
+
           <q-input
             v-model="password"
+            outlined
             dense
             label="رمز عبور"
             :type="isPwd ? 'password' : 'text'"
@@ -15,6 +22,21 @@
                 :name="isPwd ? 'visibility_off' : 'visibility'"
                 class="cursor-pointer"
                 @click="isPwd = !isPwd"
+              />
+            </template>
+          </q-input>
+          <q-input
+            v-model="repeatPassword"
+            outlined
+            dense
+            label="تکرار رمز"
+            :type="isPwdRepeat ? 'password' : 'text'"
+          >
+            <template v-slot:append>
+              <q-icon
+                :name="isPwdRepeat ? 'visibility_off' : 'visibility'"
+                class="cursor-pointer"
+                @click="isPwdRepeat = !isPwdRepeat"
               />
             </template>
           </q-input>
@@ -32,6 +54,8 @@ import { ref } from "vue";
 const username = ref(null);
 const password = ref(null);
 const ispwd = ref(true);
+const isPwdRepeat = ref(true);
+const type = ref("number");
 function onSubmit() {}
 </script>
 <style lang="scss"></style>
