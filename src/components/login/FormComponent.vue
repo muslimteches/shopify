2522bf64a676c13ff258970d8ac39639b34a2774
milad4444 <miladsarli4444@gmail.com>
2023-02-27 20:20:49 +0330

    login page

--- src/components/login/FormComponent.vue
+++ src/components/login/FormComponent.vue
@@ -58,16 +58,4 @@ function onSubmit () {
   }
 }
 </script>
-<style>
-/* Chrome, Safari, Edge, Opera */
-input::-webkit-outer-spin-button,
-input::-webkit-inner-spin-button {
-  -webkit-appearance: none;
-  margin: 0;
-}
-
-/* Firefox */
-input[type="number"] {
-  -moz-appearance: textfield;
-}
-</style>
+<style></style>
