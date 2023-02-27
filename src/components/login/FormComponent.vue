46b7ff07b96952d86f3e5a7465767c0c315887bb
milad4444 <miladsarli4444@gmail.com>
2023-02-27 20:23:10 +0330

    last change

--- src/components/login/FormComponent.vue
+++ src/components/login/FormComponent.vue
@@ -3,20 +3,9 @@
     <q-card class="my-card bg-backdrop">
       <q-card-section>
         <q-form @submit="onSubmit" class="q-gutter-md">
-          <q-input
-            outlined
-            dense
-            v-model="username"
-            :label="`نام کاربری  (${type === 'number' ? 'تلفن' : 'ایمیل'})`"
-          />
+          <q-input outlined dense v-model="username" :label="`نام کاربری  (${type === 'number' ? 'تلفن' : 'ایمیل'})`" />
           <template #append>
-            <q-btn-toggle
-              v-model="type"
-              dense
-              size="sm"
-              toggle-color="primary"
-              :options="options"
-            >
+            <q-btn-toggle v-model="type" dense size="sm" toggle-color="primary" :options="options">
               <template #1> <q-icon name="phone" /> </template>
               <template #2>
                 <q-icon name="mail" />
@@ -24,34 +13,15 @@
             </q-btn-toggle>
           </template>
 
-          <q-input
-            v-model="password"
-            outlined
-            dense
-            label="رمز عبور"
-            :type="isPwd ? 'password' : 'text'"
-          >
+          <q-input v-model="password" outlined dense label="رمز عبور" :type="isPwd ? 'password' : 'text'">
             <template v-slot:append>
-              <q-icon
-                :name="isPwd ? 'visibility_off' : 'visibility'"
-                class="cursor-pointer"
-                @click="isPwd = !isPwd"
-              />
+              <q-icon :name="isPwd ? 'visibility_off' : 'visibility'" class="cursor-pointer" @click="isPwd = !isPwd" />
             </template>
           </q-input>
-          <q-input
-            v-model="repeatPassword"
-            outlined
-            dense
-            label="تکرار رمز"
-            :type="isPwdRepeat ? 'password' : 'text'"
-          >
+          <q-input v-model="repeatPassword" outlined dense label="تکرار رمز" :type="isPwdRepeat ? 'password' : 'text'">
             <template v-slot:append>
-              <q-icon
-                :name="isPwdRepeat ? 'visibility_off' : 'visibility'"
-                class="cursor-pointer"
-                @click="isPwdRepeat = !isPwdRepeat"
-              />
+              <q-icon :name="isPwdRepeat ? 'visibility_off' : 'visibility'" class="cursor-pointer"
+                @click="isPwdRepeat = !isPwdRepeat" />
             </template>
           </q-input>
           <div>
@@ -67,28 +37,17 @@
 import { ref } from "vue";
 const username = ref(null);
 const password = ref(null);
-const ispwd = ref(true);
+const isPwd = ref(true);
 const isPwdRepeat = ref(true);
 const type = ref("number");
 const options = ref([
   { label: "", value: "number", slot: 1 },
   { label: "", value: "email", slot: 2 },
 ]);
-function onSubmit() {}
+function onSubmit () { }
 </script>
 <style lang="scss">
 .bg-backdrop {
   backdrop-filter: blur(10px);
-}
-/* Chrome, Safari, Edge, Opera */
-input::-webkit-outer-spin-button,
-input::-webkit-inner-spin-button {
-  -webkit-appearance: none;
-  margin: 0;
-}
-
-/* Firefox */
-input[type="number"] {
-  -moz-appearance: textfield;
 }
 </style>
